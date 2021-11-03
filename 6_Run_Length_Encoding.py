2a1fbd9eed0c4166b2d2c63a6dc77bdd5a3bcf9a
André Mouloken <89976036+amouloken@users.noreply.github.com>
2021-11-03 18:18:00 +0100

    Create 6_Run_Length_Encoding.py

---
+++ 6_Run_Length_Encoding.py
@@ -0,0 +1,27 @@
+# Perform Run–length encoding (RLE) data compression algorithm on string `str`
+def encode(s):
+ 
+    encoding = "" # stores output string
+ 
+    i = 0
+    while i < len(s):
+        # count occurrences of character at index `i`
+        count = 1
+ 
+        while i + 1 < len(s) and s[i] == s[i + 1]:
+            count = count + 1
+            i = i + 1
+ 
+        # append current character and its count to the result
+        encoding += str(count) + s[i]
+        i = i + 1
+ 
+    return encoding
+ 
+ 
+if __name__ == '__main__':
+ 
+   s = input("Enter your sequence:  ")  
+ 
+#   s = 'ABBCCCD'
+print(encode(s))
