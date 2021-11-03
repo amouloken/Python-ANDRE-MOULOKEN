48e4af13b7dca9c5c6d0456de498cc99393fb86f
André Mouloken <89976036+amouloken@users.noreply.github.com>
2021-11-03 18:28:57 +0100

    13_Sum_Of_Multiples.py

---
+++ 13_Sum_Of_Multiples.py
@@ -0,0 +1,25 @@
+"""Python program to find sum of
+multiples of a number up to N"""
+ 
+# Calculates sum of multiples of
+# a number upto N
+def calculate_sum(a, N):
+ 
+    # Number of multiples
+    m = N / a
+ 
+    # sum of first m natural numbers
+    sum = m * (m + 1) / 2
+ 
+    # sum of multiples
+    ans = a * sum
+ 
+    print("Sum of multiples of ", a,
+          " up to ", N, " = ", ans)
+ 
+# Driver Code
+#calculate_sum(7, 49)
+x = int(input("Enter number which you want multiple:" ))
+n = int(input("up to which number:" ))
+print(calculate_sum(x, n))
+     
