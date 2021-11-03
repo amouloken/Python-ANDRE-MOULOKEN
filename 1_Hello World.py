a1819f85ad8f6cc1fc57eadf22a8608b35e5ac1a
André Mouloken <89976036+amouloken@users.noreply.github.com>
2021-11-03 18:08:11 +0100

    Create 1_Hello World.py

---
+++ 1_Hello World.py
@@ -0,0 +1,6 @@
+the_command = input("What do you want to do,respond by Hello or enter with no action?")
+if the_command == "hello":
+    usertext = input("What is your name? ")
+    print("Hello", usertext)
+else:
+    print("Hello World", the_command)
